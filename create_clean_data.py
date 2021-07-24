d84aaf998eb157fc9846348e7911723d65f15ade
M D Ahwireng <kaaymike@hotmail.co.uk>
2021-07-24 16:15:49 +0000

    Modifies utitlity functions

--- create_clean_data.py
+++ create_clean_data.py
@@ -1,7 +1,7 @@
 from modules.utility import get_csv_data, save_csv
 from modules.clean_df import CleanData
 
-raw_data = get_csv_data('./data/AdsmartABdata.csv')
+raw_data = get_csv_data(path='./data/AdsmartABdata.csv', rev=None)
 
 clean = CleanData(raw_data)
 
@@ -9,4 +9,4 @@ clean.convert_to_date(['date'])
 
 cleaned_data = clean.remove_unanswered()
 
-save_csv(cleaned_data, './data/cleaned.csv')
+save_csv(cleaned_data, './data/AdsmartABdata.csv')
--- modules/utility.py
+++ modules/utility.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import dvc.api
 
-def get_csv_data(data_path):
+def get_csv_data(data_path, version):
+    rev=version
     print('Reading data from dvc source....')
-    data_url = dvc.api.get_url(data_path)
+    data_url = dvc.api.get_url(path=data_path, rev=rev)
     df = pd.read_csv(data_url)
     print('Reading completed, data saved as {}'.format(data_path))
     return df
