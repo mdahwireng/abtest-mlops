453b0efcd9c194e097c5cb6339e17e11844e3419
M D Ahwireng <kaaymike@hotmail.co.uk>
2021-07-24 15:44:38 +0000

    modifies utility functions and clean df

--- create_clean_data.py
+++ create_clean_data.py
@@ -1,11 +1,11 @@
 from modules.utility import get_csv_data, save_csv
 from modules.clean_df import CleanData
 
-raw_data = get_csv_data('./data/AdsmartABdaa.csv')
+raw_data = get_csv_data('./data/AdsmartABdata.csv')
 
 clean = CleanData(raw_data)
 
-clean.convert_to_date('date')
+clean.convert_to_date(['date'])
 
 cleaned_data = clean.remove_unanswered()
 
--- modules/clean_df.py
+++ modules/clean_df.py
@@ -1,4 +1,3 @@
-import dvc.api
 import pandas as pd
 
 class CleanData:
@@ -6,15 +5,20 @@ class CleanData:
         self.data = df.copy()
 
     def convert_to_date(self, cols):
+        print('Converting colums to datetime initialized...')
         data = self.data.copy()
         for col in cols:
+            print('\nConverting colums {} to datetime'.format(col))
             data[col] = pd.to_datetime(data[col])
+        print('Converting colums to datetime completed')
         self.data = data
     
     def remove_unanswered(self):
+        print('\nRemoval of anwsered surveys initialized...')
         data = self.data.copy()
         answered_index = ((data['yes'] == 1) | (data['no'] ==1))
         cleaned_df = data[answered_index]
         cleaned_df.reset_index(drop=True, inplace=True)
+        print('\nRemoval of anwsered surveys completed')
         self.data = cleaned_df
         return cleaned_df  
